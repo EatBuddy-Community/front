7913bb1d6cf04aeadeba59bb9c0e6c62cb84f716
dudska12 <skadud0113@naver.com>
2025-12-06 18:48:53 +0900

    랜딩페이지 태그 애니메이션 1차 구현

--- app/landing/Content.server.tsx
+++ app/landing/Content.server.tsx
@@ -2,23 +2,38 @@
 import { useState } from "react";
 import Tags from "../componets/DraftTag/Tags";
 import ToggleSwitchButton from "../componets/ToggleSwitch.tsx/ToggleSwitchButton";
+import { motion } from "framer-motion";
 
 export default function Content() {
-  const [seletedTag, setSeletedTag] = useState<string | null>(null);
+  const [selectedTag, setSelectedTag] = useState<string | null>(null);
+  const tags = ["전체", "메뉴1", "메뉴2", "메뉴3"];
+
   return (
     <div className="flex flex-col h-[640px] justify-between pt-5">
       <div className="flex justify-center gap-3">
         <input
           className="border-2 min-w-108 h-10 pl-4 rounded-2xl bg-white"
           type="search"
-        ></input>
+        />
         <ToggleSwitchButton />
       </div>
-      <div className="flex justify-center gap-3">
-        <Tags label="전체" />
-        <Tags label="치킨" />
-        <Tags label="족발" />
-        <Tags label="족발" />
+
+      <div className="flex justify-center gap-3 relative">
+        {tags.map((tag) => (
+          <motion.div
+            key={tag}
+            layout
+            onClick={() =>
+              setSelectedTag((prev) => (prev === tag ? null : tag))
+            }
+            className={
+              tag === selectedTag ? "absolute top-[-230px]" : "relative"
+            }
+            transition={{ type: "spring", stiffness: 300, damping: 25 }}
+          >
+            <Tags label={tag} />
+          </motion.div>
+        ))}
       </div>
     </div>
   );
